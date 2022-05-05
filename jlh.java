96e97f5d1c724a0e263255da698c97ecf2087437
cjoasis <102196653+cjoasis@users.noreply.github.com>
2022-05-04 19:05:50 -0600

    Add files via upload

---
+++ jlh.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class jlh {
+
+public static void main(String args[]){
+
+int a;
+int b=1;
+int c=0;
+
+
+Scanner captura=new Scanner (System.in);
+
+    System.out.println("Digita tus numeros (10)");
+a=captura.nextInt();
+
+
+for (int i=a; i==10; i++){
+
+
+
+
+c= c+a;
+
+
+
+b++;
+}
+
+}            
+
+}
+
